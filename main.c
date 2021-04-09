b57afaef51ab0264cb578d896ac5cb591ee565d2
ericq12 <zbruins33@gmail.com>
2021-04-09 17:04:09 -0400

    Main + readme

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <pthread.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
