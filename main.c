09123e8061fd11dd8812ddbafb1166afebf064f5
ericq12 <zbruins33@gmail.com>
2021-04-11 17:16:36 -0400

    4/11 5:15

--- main.c
+++ main.c
@@ -6,8 +6,51 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <pthread.h>
+#include "queue.c"
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  collectionPhase();
   return 0;
+}
+
+//all optional arguments required before regular args and at most once
+void collectionPhase(){
+  //set optionl arguments
+  int i = 0;
+  int directoryThreads = 1;
+  int fileThreads = 1;
+  int analysisThreads = 1;
+  char *fileSuffix = ".txt";
+  while (argv[i][0] == '-') { //loop through optional arguments
+    switch (argv[i][1]) {
+      case 'd':
+        directoryThreads = atoi(argv[i]+2);
+        if(directoryThreads==0){
+          perror("Error: ");
+          return(-1);
+        }
+        break;
+      case 'f':
+        fileThreads = atoi(argv[i]+2);
+        if(fileThreads==0){
+          perror("Error: ");
+          return(-1);
+        }
+        break;
+      case 'a':
+        analysisThreads = atoi(argv[i]+2);
+        if(analysisThreads==0){
+          perror("Error: ");
+          return(-1);
+        }
+        break;
+      case 's':
+        fileSuffix = argv[i]+2;
+        break;
+      default :
+        perror("Error: ");
+        return(-1);
+    }
+  }
+
 }
---
+++ queue.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#ifndef QSIZE
+#define QSIZE 8
+#endif
+
+typedef struct {
+	char *data[QSIZE];
+	unsigned count;
+	unsigned head;
+	int open;
+	pthread_mutex_t lock;
+	pthread_cond_t read_ready;
+	pthread_cond_t write_ready;
+} queue_t;
+
+int init(queue_t *Q)
+{
+	Q->count = 0;
+	Q->head = 0;
+	Q->open = 1;
+	pthread_mutex_init(&Q->lock, NULL);
+	pthread_cond_init(&Q->read_ready, NULL);
+	pthread_cond_init(&Q->write_ready, NULL);
+
+	return 0;
+}
+
+int destroy(queue_t *Q)
+{
+	pthread_mutex_destroy(&Q->lock);
+	pthread_cond_destroy(&Q->read_ready);
+	pthread_cond_destroy(&Q->write_ready);
+
+	return 0;
+}
+
+
+// add item to end of queue
+// if the queue is full, block until space becomes available
+int enqueue(queue_t *Q, char *item)
+{
+	pthread_mutex_lock(&Q->lock);
+
+	while (Q->count == QSIZE && Q->open) {
+		pthread_cond_wait(&Q->write_ready, &Q->lock);
+	}
+	if (!Q->open) {
+		pthread_mutex_unlock(&Q->lock);
+		return -1;
+	}
+
+	unsigned i = Q->head + Q->count;
+	if (i >= QSIZE) i -= QSIZE;
+
+	Q->data[i] = item;
+	++Q->count;
+
+	pthread_cond_signal(&Q->read_ready);
+
+	pthread_mutex_unlock(&Q->lock);
+
+	return 0;
+}
+
+
+int dequeue(queue_t *Q, char *item)
+{
+	pthread_mutex_lock(&Q->lock);
+
+	while (Q->count == 0 && Q->open) {
+		pthread_cond_wait(&Q->read_ready, &Q->lock);
+	}
+	if (Q->count == 0) {
+		pthread_mutex_unlock(&Q->lock);
+		return -1;
+	}
+
+	*item = Q->data[Q->head];
+	--Q->count;
+	++Q->head;
+	if (Q->head == QSIZE) Q->head = 0;
+
+	pthread_cond_signal(&Q->write_ready);
+
+	pthread_mutex_unlock(&Q->lock);
+
+	return 0;
+}
+
+int qclose(queue_t *Q)
+{
+	pthread_mutex_lock(&Q->lock);
+	Q->open = 0;
+	pthread_cond_broadcast(&Q->read_ready);
+	pthread_cond_broadcast(&Q->write_ready);
+	pthread_mutex_unlock(&Q->lock);
+
+	return 0;
+}
