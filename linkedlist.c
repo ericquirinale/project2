07f8f08499a42686c2df58900338b4238b4391d7
ajay-p18 <ajayparthas@gmail.com>
2021-04-11 19:33:19 -0400

    LL

---
+++ linkedlist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typdef struct{
+
+    char* word;
+    int occurences;
+    double frequency;
+    linkedlist_t *next;
+
+} linkedlist_t;
+
+
+linkedlist_t insertAlphabetically(linkedlist_t head, )
+
+
+
+
+
