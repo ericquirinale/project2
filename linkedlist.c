40e02a2bf201e40e3a1327416c4ad3e3b38c3ec9
ericq12 <zbruins33@gmail.com>
2021-04-16 16:15:44 -0400

    tests

--- linkedlist.c
+++ linkedlist.c
@@ -13,4 +13,54 @@ typedef struct linkedlist_t{
 } linkedlist_t;
 
 
-//linkedlist_t insertAlphabetically(linkedlist_t head, )
+void initLinked(linkedlist_t *ll){ //initizialing a linked list
+       // printf("%s\n", "hi22");
+    ll->word = NULL;
+    ll->occurences = 1;
+    ll->frequency = 0.0;
+    ll->next = NULL;
+     // printf("%s\n", "hi33");
+}
+
+void insertAlphabetically(linkedlist_t *ll, char *word) {
+    if(ll->word==NULL){
+      ll->word=word;
+    }
+    else{
+      for (; ll!=NULL; ll=ll->next) {
+        if(strcmp(ll->word, word)==0){
+          ll->occurences++;
+          break;
+        }
+        else if (strcmp(ll->next->word, word)<0||ll->next==NULL) {
+          linkedlist_t *new = malloc(sizeof(linkedlist_t));
+          new->word = word;
+          new->next = ll->next;
+          ll->next = new;
+          break;
+        }
+      }
+    }
+}
+
+void displayLinked(linkedlist_t *ptr) {
+
+    for (; ptr != NULL; ptr = ptr->next) {
+        printf("%s\n", ptr->word);
+    }
+}
+
+int main(void) {
+   // printf("%s\n", "hi");
+    linkedlist_t *root = malloc(sizeof(linkedlist_t));
+    initLinked(root);
+
+    insertAlphabetically(root, "apple");
+    insertAlphabetically(root, "cat");
+    insertAlphabetically(root, "bananna");
+    insertAlphabetically(root, "cat");
+
+    displayLinked(root);
+
+    return 0;
+}
